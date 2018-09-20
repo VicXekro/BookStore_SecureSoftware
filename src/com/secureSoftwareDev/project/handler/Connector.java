14ae3f3f25fba85149f0963d436eee35ce63e5f1
Holo-Buckshot <6374088+Holo-Buckshot@users.noreply.github.com>
2018-09-20 15:24:58 -0400

    setup connector

---
+++ src/com/secureSoftwareDev/project/handler/Connector.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+import org.json.HTTP;
+import org.json.JSONObject;
+
+import com.secureSoftwareDev.project.model.User;
+
+public class Connector {
+	public JSONObject main (HttpURLConnection con) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer content = new StringBuffer();
+		while ((inputLine = in.readLine()) != null) {
+				content.append(inputLine);
+		}
+		in.close();
+		JSONObject myResponse = null;
+		try {
+			myResponse = new JSONObject(content);
+			return myResponse;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public JSONObject login (Object pass) {
+		String url = "http://212.47.229.155:3005/user/login";
+		try {
+			//return main(url, "POST", pass);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	public JSONObject register (User pass) {
+		String url = "http://212.47.229.155:3005/user/register";
+		try {
+			URL target = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) target.openConnection();
+			con.setRequestMethod("POST");
+			con.setDoOutput(true);
+			con.setRequestProperty("userName", pass.userName);
+			con.setRequestProperty("password", pass.password);
+			con.setRequestProperty("email", pass.eMail);
+			con.setRequestProperty("name", pass.name);
+			con.setRequestProperty("phonenumber", pass.phoneNumber);
+			return main(con);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	public JSONObject billing (Object pass) {
+		String url = "http://212.47.229.155:3005/user/billing";
+		try {
+			//return main(url, "POST", pass);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	public JSONObject getBooks () {
+		String url = "http://212.47.229.155:3005/getBook";
+		try {
+			//return main(url, "GET", null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+}
--- src/com/secureSoftwareDev/project/model/User.java
+++ src/com/secureSoftwareDev/project/model/User.java
@@ -1,5 +1,12 @@
 package com.secureSoftwareDev.project.model;
 
 public class User {
-
+	public int id;
+	public String name;
+	public String eMail;
+	public String phoneNumber;
+	public String userName;
+	public String password;
+	
+	
 }
