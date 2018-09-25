73ee5779d34ceed907660bc9967ac2cf1b0a4e3d
Holo-Buckshot <6374088+Holo-Buckshot@users.noreply.github.com>
2018-09-25 17:04:34 -0400

    because git is a turd

--- src/com/bookstore/controls/Email.java
+++ src/com/bookstore/controls/Email.java
@@ -1,11 +1,10 @@
 package com.bookstore.controls;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Date;
-import java.util.Properties;
+import java.io.*;
+import java.util.*;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,39 +17,37 @@ import javax.mail.internet.*;
 /**
  * Servlet implementation class Email
  */
+@WebServlet("/Email")
 public class Email extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-	private String message;
+    private final String userName = "johndoe9252018@gmail.com";
+    private final String password = "Password27!";
     public Email() {
         super();
         // TODO Auto-generated constructor stub
     }
     
-    public void init() throws ServletException {
-        // Do required initialization
-        message = "Hello World";
-    }
-    
-    public void doGet(HttpServletRequest request, HttpServletResponse response/*User myUser, Order order*/) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response, Order order) throws ServletException, IOException {
     	response.setContentType("text/html");
 
         // Actual logic goes here.
         PrintWriter out = response.getWriter();
-        out.println("<h1>" + message + "</h1>");
-        final String userName = "email";
-        final String password = "password";
+        out.println("<h1>Hello World</h1>");
  
         // outgoing message information
-        String toAddress = "alexander.cardin@gmail.com";//myUser.getEmail();
+        String toAddress = order.getUser().getEmail();
         String subject = "Thank You For Your Purchase!";
-        String message = "Hello,\nThe following is a copy of your reciept from your latest purchase.\n";
+        String message = "Hello,\nThe following is a copy of your reciept from your latest purchase.\n"
+        		+ "Book Purchased: " + order.getBook().getTitle() + "\n"
+        		+ "Quantity Purchased: "+ order.getQuantityOrdered() + "\n"
+        		+ "Total Price: " + (order.getQuantityOrdered() * order.getBook().getUnitPrice());
         // sets SMTP server properties
         Properties properties = new Properties();
-        properties.put("mail.smtp.host", "outlook.office365.com");
+        properties.put("mail.smtp.host", "smpt.gmail.com");
         properties.put("mail.smtp.port", "587");
         properties.put("mail.smtp.auth", "true");
         properties.put("mail.smtp.starttls.enable", "true");
@@ -86,15 +83,6 @@ public class Email extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
- 
-    /**
-     * Test the send e-mail method
-     *
-     */
-    public static void main(String[] args) {
-        // SMTP server information
-        
     }
 
 }
